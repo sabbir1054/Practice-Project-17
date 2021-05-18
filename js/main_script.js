391107408b822756adf18b21f765c863fcfa865c
Your Name <you@example.com>
2021-05-18 19:19:05 +0600

    Web Design and responsive

---
+++ js/main_script.js
@@ -0,0 +1,7 @@
+/* Script For  Animation */
+     new WOW().init();
+/*  Gallery  */
+$(document).on("click", '[data-toggle="lightbox"]', function(event) {
+     event.preventDefault();
+     $(this).ekkoLightbox();
+   });
